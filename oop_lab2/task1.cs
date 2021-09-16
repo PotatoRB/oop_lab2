12489bb02e5974ac5d471d91181a88a80aa7cc65
PotatoRB <andrei_sml@bk.ru>
2021-09-16 17:00:54 +0300

    fitst tast ready!

---
+++ oop_lab2/task1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_lab2
+{
+    class task1
+    {
+        static void Main()
+        {
+            bool bl = true; //a
+            sbyte sbt = -128;
+            char chr = 'c';
+            int nt = 2147483647;
+            decimal dcml = 10000000000000000000;
+            double dbl = 10000000000000000000;
+            float flt = 1000000000000000;
+            uint unt = 4294967295;;
+            long lng = 9223372036854775807;
+            ulong ulng = 18446744073709551615;
+            short shrt = 32767;
+            ushort ushrt = 65535;
+            Console.WriteLine($"{bl} {sbt} {chr} {nt} {dcml} {dbl} {flt} {unt} {lng} {ulng} {shrt} {ushrt}");
+
+            byte y = 10; //b
+            short u = (short)y;
+            int i = (int)y;
+            float o = (float)i;
+            double p = (double)o;
+            Console.WriteLine(p);
+
+            byte q = 5; //b
+            short w = q;
+            int e = w;
+            float r = e;
+            double t = r;
+            Console.WriteLine(t);
+
+            object box1 = t; //c
+            double tt = (double)box1;
+
+            var kavo = 5; //d
+            int tavo = 5;
+            Console.WriteLine(kavo + tavo);
+
+            int? ttt = null; //e
+            int? yyy = 5;
+            Console.WriteLine(ttt + yyy);
+
+            var names = "ssss";
+            names = 1;
+
+            Console.ReadKey();
+        }
+    }
+}
