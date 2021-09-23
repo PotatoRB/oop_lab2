19b3fbdb596d2dcebcb6293d6cc4100e1bcb9e00
PotatoRB <andrei_sml@bk.ru>
2021-09-23 16:15:28 +0300

    sixth task ready!

---
+++ oop_lab2/task6.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace oop_lab2
+//{
+//    class task6
+//    {
+//        static void Main()
+//        {
+//            Ch();
+//            Uch();
+//            Console.ReadKey();
+//        }
+//        static void Ch()
+//        {
+//            checked 
+//            {
+//                int a = 2147483647;
+//                Console.WriteLine(a);
+//            }
+//        }
+//        static void Uch()
+//        {
+//            unchecked
+//            {
+//                int a = 2147483647;
+//                Console.WriteLine(a);
+//            }
+//        }
+//    }
+//}
