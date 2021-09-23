eea3eded12ea7b114d9cf09cbac887beb8c99124
PotatoRB <andrei_sml@bk.ru>
2021-09-23 15:22:24 +0300

    fourth task ready!

--- oop_lab2/task3.cs
+++ oop_lab2/task3.cs
@@ -1,73 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace oop_lab2
-{
-    class task3
-    {
-        static void Main()
-        {
-            int[,] arrint = { {1,2,3,4 }, {5,6,7,8 } };
-            for(int i=0; i<arrint.GetLength(0); i++)
-            {
-                for (int j=0; j<arrint.GetLength(1); j++)
-                {
-                    Console.Write(arrint[i, j]);
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
+//namespace oop_lab2
+//{
+//    class task3
+//    {
+//        static void Main()
+//        {
+//            int[,] arrint = { {1,2,3,4 }, {5,6,7,8 } };
+//            for(int i=0; i<arrint.GetLength(0); i++)
+//            {
+//                for (int j=0; j<arrint.GetLength(1); j++)
+//                {
+//                    Console.Write(arrint[i, j]);
+//                }
+//                Console.WriteLine();
+//            }
+//            Console.WriteLine();
 
-            string[] arrstr = { "first", "second", "third" };
-            foreach(string a in arrstr)
-            {
-                Console.WriteLine(a);
-            }
-            Console.WriteLine(arrstr.GetLength(0));
-            arrstr[0] = "one";
-            foreach (string a in arrstr)
-            {
-                Console.WriteLine(a);
-            }
-            Console.WriteLine();
+//            string[] arrstr = { "first", "second", "third" };
+//            foreach(string a in arrstr)
+//            {
+//                Console.WriteLine(a);
+//            }
+//            Console.WriteLine(arrstr.GetLength(0));
+//            arrstr[0] = "one";
+//            foreach (string a in arrstr)
+//            {
+//                Console.WriteLine(a);
+//            }
+//            Console.WriteLine();
 
-            int[][] arr1 = new int[3][];
-            arr1[0] = new int[2];
-            arr1[1] = new int[3];
-            arr1[2] = new int[4];
+//            int[][] arr1 = new int[3][];
+//            arr1[0] = new int[2];
+//            arr1[1] = new int[3];
+//            arr1[2] = new int[4];
 
-            for (int q = 0; q < arr1.GetLength(0); q++)
-            {
-                for (int w = 0; w < arr1[q].GetLength(0); w++)
-                {
-                    arr1[q][w]=Convert.ToInt32(Console.ReadLine());
-                }
-                Console.WriteLine();
-            }
+//            for (int q = 0; q < arr1.GetLength(0); q++)
+//            {
+//                for (int w = 0; w < arr1[q].GetLength(0); w++)
+//                {
+//                    arr1[q][w]=Convert.ToInt32(Console.ReadLine());
+//                }
+//                Console.WriteLine();
+//            }
 
-            for (int q = 0; q < arr1.GetLength(0); q++)
-            {
-                for (int w = 0; w < arr1[q].GetLength(0); w++)
-                {
-                    Console.Write($"{arr1[q][w]} ");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
+//            for (int q = 0; q < arr1.GetLength(0); q++)
+//            {
+//                for (int w = 0; w < arr1[q].GetLength(0); w++)
+//                {
+//                    Console.Write($"{arr1[q][w]} ");
+//                }
+//                Console.WriteLine();
+//            }
+//            Console.WriteLine();
 
-            var arr2 = new[] {1,2,3};
-            foreach(int x in arr2)
-            {
-                Console.Write(x+" ");
-            }
+//            var arr2 = new[] {1,2,3};
+//            foreach(int x in arr2)
+//            {
+//                Console.Write(x+" ");
+//            }
 
-            var str = "hello";
-            Console.WriteLine(str);
+//            var str = "hello";
+//            Console.WriteLine(str);
 
-            Console.ReadKey();
-        }
-    }
-}
+//            Console.ReadKey();
+//        }
+//    }
+//}
---
+++ oop_lab2/task4.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace oop_lab2
+//{
+//    class task4
+//    {
+//        static void Main()
+//        {
+//            (int nt, string str1, char ch, string str2, ulong ulng) cort = (-2, "КТО", 'Q', "ДВЕРЬ", 1000000);
+//            Console.WriteLine($"Это кортеж:{cort.nt} {cort.str1} {cort.ch} {cort.str2} {cort.ulng}");
+//            Console.WriteLine($"Выборочно:{cort.Item1} {cort.Item3} {cort.Item4}");
+//            var (one, two, three, four, five) = cort;
+//            string str3 = cort.str2;
+//            Console.WriteLine($"Распоковочка:{one}, {two}, {three}, {four}, {five}, {str3}");
+//            int diff1 = (5, 4).CompareTo((4, 5)); 
+//            int diff2 = (5, 4).CompareTo((5, 4)); 
+//            int diff3 = (4, 5).CompareTo((5, 4));
+//            Console.WriteLine($"{diff1} {diff2} {diff3}");
+
+//            Console.ReadKey();
+//        }
+//    }
+//}
